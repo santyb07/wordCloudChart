11b691cd4d49dae1cce91f7759e82aa8addd6450
Santosh Bhosale <santoshbhosale3231@gmail.com>
2022-11-06 17:01:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import axios from "axios"
+import WordCloud from "react-d3-cloud";
+
 
 function App() {
+  const [twitterData,setTwitterData] = useState();
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const res = await axios.get("http://localhost:3500/stats");
+      const data= res.data;
+      setTwitterData(data);
+      console.log(data);
+    }
+    fetchData();
+  },[])
+
+  const hashTagsWithValue = twitterData?.twitter?.timelineStats?.timeline[0].hashtags
+  console.log(hashTagsWithValue)
+
+  // const hashtags = Object?.keys(hashTagsWithValue).map((value)=>{
+  //   console.log(hashtags)
+  // })
+ 
+
+
+const data=[{
+  text:'hello',
+  value:1000
+},
+{
+  text:'hello',
+  value:1000
+}
+]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WordCloud
+    width={1000}
+    height={750}
+    data={data}
+    fontSize={100}
+    rotate={150}
+    padding={2}
+    />
   );
 }
 
